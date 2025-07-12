c19dd21b424594469087138c760e8b97c6d6d572
raghavrao14 <s.raghav.rao14@gmail.com>
2025-07-11 23:34:18 -0400

    Update Todo.java

--- src/Todo.java
+++ src/Todo.java
@@ -1,91 +1,70 @@
-import stdlib.StdOut;
+public class mergeSort {
 
-public class toDo {
-    toDoItem head;
+    // Sorts by urgency and isDone
+    public static void sort(toDoItem[] a) {
+        toDoItem[] aux = new toDoItem[a.length];
+        sort(a, aux, 0, a.length - 1);
+    }
 
-    public void add(String task, String deadline, String urgency) {
-        toDoItem newItem = new toDoItem(task, deadline, urgency);
-        if (head == null) head = newItem;
-        else {
-            toDoItem current = head;
-            while (current.next != null) current = current.next;
-            current.next = newItem;
+    private static void sort(toDoItem[] a, toDoItem[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    // Merges by isDone first, then urgency
+    private static void merge(toDoItem[] a, toDoItem[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) aux[k] = a[k];
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (!aux[i].isDone && aux[j].isDone) a[k] = aux[i++];
+            else if (aux[i].isDone && !aux[j].isDone) a[k] = aux[j++];
+            else if (urgencyVal(aux[j].urgency) < urgencyVal(aux[i].urgency)) a[k] = aux[j++];
+            else a[k] = aux[i++];
         }
     }
 
-    public void delete(int index) {
-        if (head == null || index < 1) return;
-        if (index == 1) {
-            head = head.next;
-            return;
+    // Converts urgency code to priority number
+    private static int urgencyVal(String urgency) {
+        return switch (urgency) {
+            case "R" -> 1;
+            case "Y" -> 2;
+            case "G" -> 3;
+            default -> 4;
+        };
+    }
+
+    // Sorts by deadline and isDone
+    public static void sortByDeadline(toDoItem[] a) {
+        toDoItem[] aux = new toDoItem[a.length];
+        sortByDeadline(a, aux, 0, a.length - 1);
+    }
+
+    private static void sortByDeadline(toDoItem[] a, toDoItem[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sortByDeadline(a, aux, lo, mid);
+        sortByDeadline(a, aux, mid + 1, hi);
+        mergeByDeadline(a, aux, lo, mid, hi);
+    }
+
+    // Merges by isDone first, then by deadline (earlier first)
+    private static void mergeByDeadline(toDoItem[] a, toDoItem[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) aux[k] = a[k];
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (!aux[i].isDone && aux[j].isDone) a[k] = aux[i++];
+            else if (aux[i].isDone && !aux[j].isDone) a[k] = aux[j++];
+            else if (aux[i].getDeadlineDate().isBefore(aux[j].getDeadlineDate())) a[k] = aux[i++];
+            else a[k] = aux[j++];
         }
-        toDoItem current = head;
-        for (int i = 1; current.next != null && i < index - 1; i++) current = current.next;
-        if (current.next != null) current.next = current.next.next;
-    }
-
-    public void markDone(int index) {
-        toDoItem current = head;
-        for (int i = 1; current != null && i < index; i++) current = current.next;
-        if (current != null) current.isDone = true;
-    }
-
-    public void printList() {
-        toDoItem current = head;
-        int index = 1;
-        while (current != null) {
-            StdOut.println(index + ". [" + (current.isDone ? "X" : " ") + "] " + current.task +
-                    ", Deadline: " + current.deadline + ", Urgency: " + current.urgency);
-            current = current.next;
-            index++;
-        }
-    }
-
-    public void sort() {
-        int size = getSize();
-        if (size < 2) return;
-
-        toDoItem[] arr = toArray(size);
-        mergeSort.sort(arr);
-        rebuildFromArray(arr);
-    }
-
-    public void sortByDeadline() {
-        int size = getSize();
-        if (size < 2) return;
-
-        toDoItem[] arr = toArray(size);
-        mergeSort.sortByDeadline(arr);
-        rebuildFromArray(arr);
-    }
-
-    private int getSize() {
-        int count = 0;
-        toDoItem current = head;
-        while (current != null) {
-            count++;
-            current = current.next;
-        }
-        return count;
-    }
-
-    private toDoItem[] toArray(int size) {
-        toDoItem[] arr = new toDoItem[size];
-        toDoItem current = head;
-        for (int i = 0; i < size; i++) {
-            arr[i] = current;
-            current = current.next;
-        }
-        return arr;
-    }
-
-    private void rebuildFromArray(toDoItem[] arr) {
-        head = arr[0];
-        toDoItem current = head;
-        for (int i = 1; i < arr.length; i++) {
-            current.next = arr[i];
-            current = current.next;
-        }
-        current.next = null;
     }
 }
